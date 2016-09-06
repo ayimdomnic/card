b210fa8bf5c8bff06b45398ae8351beaff1aca6d
Ayimdomnic <Ayimdomnic@statuscreative.co.ke>
2016-09-06 00:47:05 -0700

    Factory:: we can generate a single valid card

--- app/Creditcard.php
+++ app/Creditcard.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Creditcard extends Model
 {
 
-    public function __construct(Error $e)
-    {
-        $e = "Please Provide a valid card ref";
-    }
+    // public function __construct(Error $e)
+    // {
+    //     $e = "Please Provide a valid card ref";
+    // }
 
     protected $fillable = [
         'user_id',
@@ -31,19 +31,19 @@ class Creditcard extends Model
        return $this->belongsTo('App\User');
     }
 
-    public function getValidatedAttribute($cvv)
-    {
-        $card = Input::getallheaders($cvv)
+    // public function getValidatedAttribute($cvv)
+    // {
+    //     $card = Input::getallheaders($cvv)
 
-        if (!isset($cvv)) {
-            # code...
-            foreach ($cvv as $key => $value) {
-                # code...
-                return $cvv
+    //     if (!isset($cvv)) {
+    //         # code...
+    //         foreach ($cvv as $key => $value) {
+    //             # code...
+    //             return $cvv
 
-            }
-        }else{
-            return $e;
-        }
-    }
+    //         }
+    //     }else{
+    //         return $e;
+    //     }
+    // }
 }
